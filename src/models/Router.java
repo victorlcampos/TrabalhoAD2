b31956f3a3c1d199934ade16a588e8382fc05978
filipe <filipe@192.168.0.105>
2013-08-11 12:23:11 -0300

    Java doc comments on Router

--- src/models/Router.java
+++ src/models/Router.java
@@ -10,17 +10,49 @@ import Enum.EventType;
 import Enum.RouterType;
 import Utils.SimulatorProperties;
 
+/**
+ * 
+ * Implementação da classe que irá simular o roteador.
+ * <p>
+ * Sua função será representar o buffer por onde passam nossas sessões TCP,
+ * sendo ele o gargalo na rota IP destas sessões.
+ *
+ */
 public class Router implements Listener {
+
+	/**
+	 * Lista que representa a fila de espera.
+	 */
 	private List<Event> eventBuffer;
-	/** Tamanho do buffer em pacotes */
+
+	/** 
+	 * Tamanho do buffer em pacotes 
+	 */
 	private Integer bufferSize;
-	/** Taxa com que a fila é esvaziada em bytes */
+	
+	/** 
+	 * Taxa com que a fila é esvaziada em bytes 
+	 */
 	private Long broadcastRate;
 
+	/**
+	 * Política de fila que está sendo usada.
+	 */
 	private RouterType type;
+
+	/**
+	 * Flag para indicar se o roteador está em serviço.
+	 */
 	private Boolean onService;
 	
+	/**
+	 * Referencia para instancia única do simulator
+	 */
 	private Simulator simulator;
+	
+	/**
+	 * Armazena o tempo da última vez que entregou um pacote ao receptor
+	 */
 	private Long lastTimeDelivered;
 	
 	/**
@@ -63,6 +95,14 @@ public class Router implements Listener {
 	 */
 	private Long lastBusyPeriodTime;
 
+	/**
+	 * Constrói um roteador com a taxa fornecida.
+	 * Ele irá escutar os eventos do tipo <code>EventType.PACKAGE_SENT</code> e <code>EventType.PACKAGE_DELIVERED</code>.
+	 * 
+	 * @param broadcastRate taxa de saída do roteador. 
+	 * @param type tipo de politica de descarte
+	 * @param bufferSize tamanho do buffer
+	 */
 	public Router(Integer bufferSize, Long broadcastRate, RouterType type) {
 		super();
 		this.bufferSize = bufferSize;
@@ -88,6 +128,14 @@ public class Router implements Listener {
 		lastBusyPeriodTime =  0l;
 	}
 
+	/**
+	 * Implementação do método responsável por escutar os eventos.
+	 * <p>
+	 * Ele irá identificar o tipo de evento em questão, 
+	 * e então executar as tarefas correspondentes ao tipo de evento e também à política de descarte.
+	 * 
+	 *  @param evento que será escutado.
+	 */
 	@Override
 	public void Listen(Event event) {
 		switch (event.getType()) {
@@ -135,6 +183,13 @@ public class Router implements Listener {
 		}
 	}
 
+	/**
+	 * Recebe o pacote ao chegar na fila, caso não tenha que ser descartado devido à política de descarte.
+	 * <p>
+	 * Se houverem pacotes na fila, coloca o novo evento na fila, senão, inicia o atendimento imediatamente, chamando o método deliverPackage()
+	 * 
+	 * @param event evento com pacote que acabou de chegar na fila
+	 */
 	private void acceptPackage(Event event) {
 		if(onService) {
 			if (eventBuffer.size() < bufferSize) {
@@ -146,6 +201,10 @@ public class Router implements Listener {
 		}
 	}
 
+	/**
+	 * Dispara evento de entrega do pacote ao receptor para acontecer após o tempo de serviço
+	 * @param event
+	 */
 	private void deliverPackage(Event event) {
 		Long initialTime = Math.max(lastTimeDelivered, event.getTime());
 		
