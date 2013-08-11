e422f8c91b8c2820b7cc8f4808b8cd9c51dc6f14
filipe <filipe@192.168.0.105>
2013-08-11 16:01:43 -0300

    Java doc comments on BackgroundTraffic, ServerGroup, Listene e ServerStatus

--- src/models/ServerGroup.java
+++ src/models/ServerGroup.java
@@ -1,21 +1,43 @@
 package models;
 
+/**
+ * 
+ * Implementação de um grupo de servidores de um mesmo tipo.
+ * <p>
+ * Esta classe irá representar um tipo de grupo dos servidores sendo estudados.
+ * Seu objetivo é agrupar servidores de um mesmo tipo, armazenando para este tipo a
+ * informação acerca do atraso o qual um pacote sofre, quando é enviado por um servidor deste tipo.
+ *
+ */
 public class ServerGroup {
+	
+	/**
+	 * Tempo que um pacote demora desde o momento em que ele sai do servidor, até chegar ao roteador.
+	 */
 	private Long delay;
 
+	/**
+	 * Inicializa um grupo de servidores que compartilham o mesmo atraso informado. 
+	 * 
+	 * @param delay atraso que um pacote sofre para ir do servidor até o roteador.
+	 */
 	public ServerGroup(Long delay) {
 		super();
 		this.delay = delay;
 	}
 
+	/**
+	 * Retorna o atraso que um pacote sofre para ir do servidor até o roteador, quando ele for enviado por um servidor deste grupo.
+	 * 
+	 * @return tempo que um pacote demora desde o momento em que ele sai do servidor, até chegar ao roteador.
+	 */
 	public Long getDelay() {
 		return delay;
 	}
 
-	public void setDelay(Long delay) {
-		this.delay = delay;
-	}
-	
+	/**
+	 * Retorna uma string que representa e identifica o grupo, através de seu delay
+	 */
 	@Override
 	public String toString() {
 		return "Grupo "+delay;
--- src/models/interfaces/Listener.java
+++ src/models/interfaces/Listener.java
@@ -2,6 +2,18 @@ package models.interfaces;
 
 import models.Event;
 
+/**
+ *
+ * Interface que deverá ser implementada por qualquer
+ * classe que queira escutar os eventos da simulação.
+ *
+ */
 public interface Listener {
+
+	/**
+	 * Método a ser implementado que irá manipular os eventos recebidos pelo por esse <code>Listener</code>.
+	 * 
+	 * @param event evento a ser tratado.
+	 */
 	public void Listen(Event event);
 }
